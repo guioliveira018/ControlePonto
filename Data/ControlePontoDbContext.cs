6e9a132c48028095f7ab1321e034c6e351a690cd
Aluno <Aluno@DESKTOP-DMGHJ3C>
2024-03-12 21:58:24 -0300

    Adicionar arquivos de projeto.

---
+++ Data/ControlePontoDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlePonto.Data
+{
+    internal class ControlePontoDbContext : DbContext
+    {
+        public ControlePontoDbContext(DbContextOptions<ControlePontoDbContext> opitions) : base (opitions)
+        {
+
+        }
+
+        public 
+    }
+
+}
